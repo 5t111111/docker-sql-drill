3fdf3a91a1ac36d28c42594d92b9d3127f519d47
Hirofumi Wakasugi <baenej@gmail.com>
2016-09-24 17:56:49 +0900

    Update

---
+++ docker-entrypoint-initdb-mysql.d/create_sql_drill_on_mysql.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# run_mysql="docker-compose run --rm mysql"
+# 
+# $run_mysql echo "CREATE DATABASE sql_drill DEFAULT CHARSET utf8;" | mysql -uroot
+# $run_mysql echo "GRANT ALL PRIVILEGES ON sql_drill.* TO 'sql_drill'@'%' IDENTIFIED BY 'sql_drill'; FLUSH PRIVILEGES" | mysql -uroot
+# 
+# $run_mysql cat /create_for_mysql.sql | mysql -usql_drill -psql_drill sql_drill
+# $run_mysql cat /data.sql | mysql -usql_drill -psql_drill sql_drill
+# 
+
+
+echo "CREATE DATABASE sql_drill DEFAULT CHARSET utf8;" | mysql -uroot
+echo "GRANT ALL PRIVILEGES ON sql_drill.* TO 'sql_drill'@'%' IDENTIFIED BY 'sql_drill'; FLUSH PRIVILEGES" | mysql -uroot
+cat /create_for_mysql.sql | mysql -usql_drill -psql_drill sql_drill
+cat /data.sql | mysql -usql_drill -psql_drill sql_drill
