dcdb76d3583dbb6d3122a6eccf9f6239bece9483
Hirofumi Wakasugi <baenej@gmail.com>
2016-09-25 15:56:31 +0900

    Fix create_redash_database script for docker-compose version 2

--- create_redash_database.sh
+++ create_redash_database.sh
@@ -11,18 +11,12 @@ $run_redash /opt/redash/current/manage.py database create_tables
 # Create default admin user
 $run_redash /opt/redash/current/manage.py users create --admin --password admin "Admin" "admin"
 
-# This is a hack to get the Postgres IP and PORT from the instance itself.
-temp_env_file=$(mktemp /tmp/pg_env.XXXXXX || exit 3)
-docker-compose run --rm postgres env > "$temp_env_file"
-source "$temp_env_file"
-
-run_psql="docker-compose run --rm postgres psql -h $POSTGRES_PORT_5432_TCP_ADDR -p $POSTGRES_PORT_5432_TCP_PORT -U postgres"
+run_psql="docker-compose run --rm postgres psql -h postgres -p 5432 -U postgres"
 
 # Create redash_reader user. We don't use a strong password, as the instance supposed to be accesible only from the redash host.
 $run_psql -c "CREATE ROLE redash_reader WITH PASSWORD 'redash_reader' NOCREATEROLE NOCREATEDB NOSUPERUSER LOGIN"
-$run_psql -c "grant select(id,name,type) ON data_sources to redash_reader;"
-$run_psql -c "grant select(id,name) ON users to redash_reader;"
-$run_psql -c "grant select on events, queries, dashboards, widgets, visualizations, query_results to redash_reader;"
+$run_psql -c "GRANT select(id,name,type) ON data_sources TO redash_reader;"
+$run_psql -c "GRANT select(id,name) ON users TO redash_reader;"
+$run_psql -c "GRANT select ON events, queries, dashboards, widgets, visualizations, query_results TO redash_reader;"
 
 $run_redash /opt/redash/current/manage.py ds new "re:dash metadata" --type "pg" --options "{\"user\": \"redash_reader\", \"password\": \"redash_reader\", \"host\": \"postgres\", \"dbname\": \"postgres\"}"
-
