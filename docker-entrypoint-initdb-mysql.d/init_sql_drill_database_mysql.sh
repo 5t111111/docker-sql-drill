49875f182f12aa0f845e5cd6b9f6c00fd6f40274
Hirofumi Wakasugi <baenej@gmail.com>
2016-09-25 16:34:21 +0900

    Fix MySQL DB charcode to utf8

--- docker-entrypoint-initdb-mysql.d/init_sql_drill_database_mysql.sh
+++ docker-entrypoint-initdb-mysql.d/init_sql_drill_database_mysql.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-echo "CREATE DATABASE sql_drill DEFAULT CHARSET utf8;" | mysql -uroot
+echo "CREATE DATABASE sql_drill DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -uroot
 echo "GRANT ALL PRIVILEGES ON sql_drill.* TO 'sql_drill'@'%' IDENTIFIED BY 'sql_drill'; FLUSH PRIVILEGES" | mysql -uroot
 cat /create_for_mysql.sql | mysql -usql_drill -psql_drill sql_drill
 cat /data.sql | mysql -usql_drill -psql_drill sql_drill
